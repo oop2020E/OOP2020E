dbfc8f743d512c64f62dc9ba927f910ec3707ec6
Thomas <thomas@glirc.dk>
2020-09-04 10:51:31 +0100

    auto_2020-09-04_11:51

--- KG01_intro/Program.cs
+++ KG01_intro/Program.cs
@@ -9,11 +9,20 @@ namespace KG01_intro.Circle
     //INTROTODO 30 Indkapsling -- fra GetV/SetV() til Properties
 
     //TODOEXTRA 50 Enum+switch
+    class Document
+    {
+        public int Linier;
+    }
+
     class Printer
     {
-        public void Print(string text)
+        public void Print(Document text)
         {
-            // ...
+            for(int i = 0; i < text.Linier; i++)
+            {
+                //Console.WriteLine();
+                Drej(1);
+            }
         }
         public void Drej(int linier)
         {
@@ -27,8 +36,8 @@ namespace KG01_intro.Circle
         static void Main(string[] args)
         {
             Printer printer = new Printer();
-            printer.Drej();
-            printer.Print("Hej");
+            printer.Print(new Document());
+            printer.Drej(1);
         }
     }
 }

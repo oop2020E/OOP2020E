ab7a1cda66aad675260b60308aa132fcef4fedbf
Thomas Bøgholm <github.com@glirc.dk>
2020-09-09 12:51:46 +0200

    Create KG02 and remove KG01

--- KG01_intro/Program.cs
+++ KG01_intro/Program.cs
@@ -4,24 +4,11 @@ using System.Threading.Tasks;
 
 namespace KG01_intro
 {
-
-    //INTROTODO 15 Discuss memory
-    //INTROTODO 20 Create class: fields, accessmodifiers -- printer-driver
-    //INTROTODO 30 Indkapsling -- fra GetV/SetV() til Properties
-    
-
-    //TODOEXTRA 50 Enum+switch
-
-
-
-
     class Program
     {
         static void Main(string[] args)
         {
             
         }
-
-
     }
 }

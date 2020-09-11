a6a6e68ad27d2a651552e1e39ef2b0f01847a4b6
Thomas <thomas@glirc.dk>
2020-09-11 10:15:00 +0100

    auto_2020-09-11_11:15

--- KG02_inheritance/Figur.cs
+++ KG02_inheritance/Figur.cs
@@ -2,6 +2,15 @@
 {
     public abstract class Figur
     {
+        public Figur()
+        {
+
+        }
+        public Figur(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
         public int X { get; set; }
         public int Y { get; set; }
         public abstract int Area();

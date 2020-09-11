c48ced115dfe3243334aa5beccfb8239563087ed
Thomas <thomas@glirc.dk>
2020-09-11 10:12:55 +0100

    auto_2020-09-11_11:12

--- KG02_inheritance/Cirkel.cs
+++ KG02_inheritance/Cirkel.cs
@@ -4,6 +4,12 @@ namespace KG02_inheritance
 {
     class Cirkel : Figur
     {
+        public Cirkel(int radius, int x, int y)
+        {
+            Radius = radius;
+            X = x;
+            Y = y;
+        }
         public int Radius { get; set; }
         public override int Area()
         {

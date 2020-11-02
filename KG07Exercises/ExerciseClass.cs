5fa51eeafbec8f9b03d095888ea3e2e668869253
boegholm <bitbucket.org@glirc.dk>
2020-11-02 13:03:45 +0100

    Exercises for KG07

---
+++ KG07Exercises/ExerciseClass.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KG07Exercises
+{
+
+    #region Exercise 1
+    public delegate string StringJoin(string s1, string s2);
+
+    #endregion
+
+    public partial class ExerciseClass
+    {
+        #region Exercise 2
+
+        public string Join3(string s1, string s2, string s3, StringJoin f) => f(s1, f(s2, s3));
+        #endregion
+
+        public string JoinAllStrings(List<string> l, StringJoin f) => JoinAllStrings(l, f, 0);
+        private string JoinAllStrings(List<string> l, StringJoin f, int startIndex) => JoinAll(l, ( v1,  v2)=>f(v1, v2), startIndex);
+/***        after completing exercise 4, we replace this
+        { 
+
+        if (l.Count == 0 || l.Count <= startIndex)
+            throw new ArgumentException("Invalid startindex or no elements in list");
+        if (l.Count - startIndex == 1)
+            return l[startIndex];
+        else return f(l[startIndex], JoinAllStrings(l, f, startIndex + 1));
+        } 
+**/
+
+}
+
+    #region Exercise 4
+    public delegate T Join<T>(T v1, T v2);
+    public partial class ExerciseClass
+    {
+        public T JoinAll<T>(List<T> l, Join<T> f) => JoinAll(l, f, 0);
+
+        private T JoinAll<T>(List<T> l, Join<T> f, int startIndex)
+        {
+            if (l.Count == 0 || l.Count <= startIndex)
+                throw new ArgumentException("Invalid startindex or no elements in list");
+            if (l.Count - startIndex == 1)
+                return l[startIndex];
+            else return f(l[startIndex], JoinAll(l, f, startIndex + 1));
+        }
+
+        #region Exercise 5
+
+        public bool Exists<T>(Predicate<T> f, T[] a)
+        {
+            foreach (var item in a)
+            {
+                if (f(item))
+                    return true;
+            }
+            return false;
+        }
+        #endregion
+
+        #region Exercise 6
+        public T Twice<T>(Func<T, T> f, T v) => f(f(v));
+        #endregion
+    }
+    #endregion
+
+}
---
+++ KG07ExercisesTests/ExerciseClassTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KG07Exercises;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KG07Exercises.Tests
+{
+    [TestClass()]
+    public class ExerciseClassTests
+    {
+        [TestMethod]
+        public void JoinAllStringsTest_abcWithUnderscoreFunction_a_b_c()
+        {
+            List<string> strings = new List<string>() { "a", "b", "c" };
+            ExerciseClass c = new ExerciseClass();
+            string expected = "a_b_c";
+            string joinfunction(string s1, string s2) => $"{s1}_{s2}";
+
+            var result = c.JoinAllStrings(strings, joinfunction);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void JoinAllStringsTest_joinEmptyString_ThrowsException()
+        {
+            List<string> emptyList = new List<string>();
+            ExerciseClass c = new ExerciseClass();
+
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                c.JoinAllStrings(emptyList, (s1, s2) => s1 + s2);
+            });
+        }
+
+        [TestMethod]
+        public void TwiceTest_ExampleFromExercise_WorksAsSpecifiedInExercise()
+        {
+            ExerciseClass c = new ExerciseClass();
+            int inputFunction(int x) => x * 2;
+            int expected = 4;
+            int inputvalue = 1;
+
+            int result = c.Twice(inputFunction, inputvalue);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}

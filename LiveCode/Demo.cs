5d3f05f2e11c11b92693a67f8c3acad25770f2cf
Thomas Bøgholm <github.com@glirc.dk>
2020-11-06 12:08:46 +0100

    Livecode done

--- LiveCode/Demo.cs
+++ LiveCode/Demo.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
@@ -24,8 +25,26 @@ employeeID lastName    firstName title   titleOfCourtesy birthDate   hireDate ad
         //etc
     }
 
+    //class InfNumbers : IEnumerable<int>
+    //{
+    //    public IEnumerator<int> GetEnumerator()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    IEnumerator IEnumerable.GetEnumerator()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+
+
     public class Employee //employeeID,lastName,firstName,title,titleOfCourtesy,birthDate,hireDate,address,city,region,postalCode,country,homePhone,extension,photo,notes,reportsTo,photoPath
     {
+        public override string ToString()
+        {
+            return Id + " " +Firstname + " " + Lastname;
+        }
         public int Id { get; set; }
         public string Lastname { get; set; }
         public string Firstname { get; set; }
@@ -38,6 +57,22 @@ employeeID lastName    firstName title   titleOfCourtesy birthDate   hireDate ad
     public class Demo
     {
 
+        IEnumerable<long> GetInfSequence()
+        {
+            long l = 0;
+            yield return l++;
+            yield return l++;
+            yield return l++;
+            yield return l++;
+            yield return l++;
+            yield return l++;
+            yield return l++;
+            yield return l++;
+            yield return l++;
+            yield return l++;
+        }
+
+
         const string EmployeesUrl = "https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/csv/employees.csv";
         const string ProductsUrl = "https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/csv/products.csv";
 
@@ -45,7 +80,7 @@ employeeID lastName    firstName title   titleOfCourtesy birthDate   hireDate ad
         public IEnumerable<string> DownloadAsLines(Uri url)
         {
             WebClient wc = new WebClient();
-            return BytesToLines(wc.DownloadData(url));                        
+            return BytesToLines(wc.DownloadData(url));
         }
         private IEnumerable<string> BytesToLines(byte[] bytes)
         {
@@ -65,13 +100,60 @@ employeeID lastName    firstName title   titleOfCourtesy birthDate   hireDate ad
             return File.ReadAllLines(f);
         }
 
-        //public IEnumerable<Employee> Employees => ReadLinesFromFile("employees.csv")
+        public IEnumerable<Employee> Employees {
+            get {
+                
+                return ReadLinesFromFile("employees.csv").Skip(1)
+                    .Where(v => { Console.WriteLine("kaldt"); return true; })
+                    .Select(s => s.Split(','))
+                    .Select(sa => arrayToEmployee(sa))
+                    ;
+            }
+        }
 
-        Employee arrayToEmployee(string[] sa) => new Employee() { Id = int.Parse(sa[0]), City = sa[8], Firstname = sa[2], Lastname = sa[1], PostalCode = sa[10], Title = sa[3], Country = sa[11] };
+        Employee arrayToEmployee(string[] sa) => new Employee() { 
+            Id = int.Parse(sa[0]), 
+            City = sa[8], 
+            Firstname = sa[2], 
+            Lastname = sa[1],
+            PostalCode = sa[10], 
+            Title = sa[3], 
+            Country = sa[11] 
+        };
 
         public void Run()
         {
-            
+            var intlist = new List<int>() { 2, 4, 6 };
+
+
+            foreach (var item in GetInfSequence().Concat(GetInfSequence()))
+            {
+                Console.WriteLine(item);
+
+            }
+
+            List<Employee> amerikanere = Employees.Take(2).ToList();
+            //.Join(intlist,  e => e.Id, i => i, (e, i) => new { 
+            //    Fullname=$"{e.Firstname} {e.Lastname}", 
+            //    Id=i }); 
+            //.Where(v => v.Country != "USA")
+            //.GroupBy(v => v.Country)
+
+            //.SelectMany(g => g);
+
+
+            //Console.WriteLine(v);
+            Console.WriteLine("første iteration");
+            foreach (var item in amerikanere)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Anden iteration");
+            foreach (var item in amerikanere)
+            {
+                Console.WriteLine(item);
+            }
+
         }
     }
 }

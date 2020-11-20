bd0efeaf134cf6828eda4e41836b4cccd9f11f06
Thomas Bøgholm <github.com@glirc.dk>
2020-11-20 09:37:36 +0100

    initial

--- LiveCode/Demo.cs
+++ LiveCode/Demo.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
@@ -9,69 +10,68 @@ using System.Text;
 
 namespace LiveCode
 {
-    /*
-0	        1	       2	      3 	        4           	5       	6	        7               	8       	9
-productID productName supplierID categoryID  quantityPerUnit unitPrice   unitsInStock unitsOnOrder    reorderLevel discontinued
-0	            1	       2     3	        4	            5	         6  	7	    8	  9 	    10  	11	    12  	13      	14  	15  	16  	17
-employeeID lastName    firstName title   titleOfCourtesy birthDate   hireDate address city region  postalCode country homePhone extension   photo notes   reportsTo photoPath
+    //interface 
 
-    */
-    public class Products // productID,productName,supplierID,categoryID,quantityPerUnit,unitPrice,unitsInStock,unitsOnOrder,reorderLevel,discontinued
+    interface ICommand
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public decimal  UnitPrice { get; set; }
-        //etc
+        void Execute();
     }
 
-    public class Employee //employeeID,lastName,firstName,title,titleOfCourtesy,birthDate,hireDate,address,city,region,postalCode,country,homePhone,extension,photo,notes,reportsTo,photoPath
+    class Controllable
     {
-        public int Id { get; set; }
-        public string Lastname { get; set; }
-        public string Firstname { get; set; }
-        public string Title { get; set; }
-        public string City { get; set; }
-        public string PostalCode { get; set; }
-        public string Country { get; internal set; }
+        private void Refresh()
+        {
+            Console.Clear();
+
+            Console.SetCursorPosition(10, pos);
+            Console.WriteLine("X");
+        }
+        public ICommand MoveUpCommand => new MoveUpCommandImpl(this);
+        public ICommand MoveDownCommand => new MoveDownCommandImpl(this);
+
+        int pos = 0;
+
+        private class MoveUpCommandImpl : ICommand
+        {
+            private Controllable controllable;
+
+            public MoveUpCommandImpl(Controllable controllable)
+            {
+                this.controllable = controllable;
+            }
+
+            public void Execute()
+            {
+                controllable.pos++;
+            }
+        }
+
+        private class MoveDownCommandImpl : ICommand
+        {
+            private Controllable controllable;
+
+            public MoveDownCommandImpl(Controllable controllable)
+            {
+                this.controllable = controllable;
+            }
+
+            public void Execute()
+            {
+                controllable.pos--;
+            }
+        }
     }
+
+    class App
+    {
+
+    }
+
 
     public class Demo
     {
-
-        const string EmployeesUrl = "https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/csv/employees.csv";
-        const string ProductsUrl = "https://raw.githubusercontent.com/graphql-compose/graphql-compose-examples/master/examples/northwind/data/csv/products.csv";
-
-        #region UrlReading
-        public IEnumerable<string> DownloadAsLines(Uri url)
-        {
-            WebClient wc = new WebClient();
-            return BytesToLines(wc.DownloadData(url));                        
-        }
-        private IEnumerable<string> BytesToLines(byte[] bytes)
-        {
-            var result = new List<string>();
-            using (StreamReader sr = new StreamReader(new MemoryStream(), Encoding.Default))
-            {
-                string line;
-                while ((line = sr.ReadLine()) != null)
-                    result.Add(line);
-            }
-            return result;
-        }
-        #endregion
-
-        public IEnumerable<string> ReadLinesFromFile(string f)
-        {
-            return File.ReadAllLines(f);
-        }
-
-        //public IEnumerable<Employee> Employees => ReadLinesFromFile("employees.csv")
-
-        Employee arrayToEmployee(string[] sa) => new Employee() { Id = int.Parse(sa[0]), City = sa[8], Firstname = sa[2], Lastname = sa[1], PostalCode = sa[10], Title = sa[3], Country = sa[11] };
-
         public void Run()
         {
-            
         }
     }
 }
